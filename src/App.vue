75c48bd5f5ae0fc948211a1f426ea7ee0affaa77
Moazam05 <salmanmoazam08@gmail.com>
2024-02-21 20:27:42 +0500

    routes define

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam quidem
+    cupiditate perferendis, eveniet nemo minus mollitia officiis suscipit
+    repellendus architecto laborum labore dolorum obcaecati vero! Non suscipit
+    nemo iusto maiores.
+  </div>
+  <router-view />
 </template>
