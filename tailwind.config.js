0222a63bbc301466565c8286eb8333a6eaebddb0
Moazam05 <salmanmoazam08@gmail.com>
2024-02-21 20:35:15 +0500

    react icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "12rem",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      body: "#F5F5F9",
+      "body-2": "#F5F6F8",
+      gray: "#697A8D",
+      "gray-100": "#8E9BAA",
+      "gray-200": "#566A7F",
+      "gray-300": "#D9DEE3",
+      "gray-400": "#B4BDC6",
+      "gray-500": "#ECEEF1",
+      blue: "#696CFF",
+      "blue-100": "#4F52E6",
+      "blue-200": "#F3F3FF",
+      "purple-100": "#E7E7FF",
+      orange: "#f57224",
+      cyan: "#06b6d4",
+      "cyan-50": "rgba(7, 182, 213, 0.15)",
+    },
+    fontFamily: {
+      sans: ["Public Sans", "sans-serif"],
+    },
+    extend: {
+      boxShadow: {
+        sidebar: "1px 1px 8px rgba(219, 213, 226, 0.80)",
+        mobileSidebar: "2px 0px 5px 0px rgba(0,0,0,0.1)",
+        card: "1px 1px 8px 0px #DBD5E2CC",
+      },
+    },
   },
   plugins: [],
 };
