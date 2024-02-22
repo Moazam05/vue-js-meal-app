c8da091a6e2d2cbd733c74e52fe475efdbc6e2d8
Moazam05 <salmanmoazam08@gmail.com>
2024-02-22 14:06:05 +0500

    vuex store setup

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/aura-light-green/theme.css";
+import store from "./redux/store";
 
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
-
+app.use(store);
 app.mount("#app");
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>Home</div>
+
+  <pre>{{ meals }}</pre>
 </template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../../redux/store";
+
+const meals = computed(() => store.state.meals);
+</script>
 
 <script>
 export default {
