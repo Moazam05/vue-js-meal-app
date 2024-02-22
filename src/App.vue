a9cd922a1bfe8524acfa24016aa27fd11fe1ab13
Moazam05 <salmanmoazam08@gmail.com>
2024-02-22 11:20:48 +0500

    prime vue setup

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     cupiditate perferendis, eveniet nemo minus mollitia officiis suscipit
     repellendus architecto laborum labore dolorum obcaecati vero! Non suscipit
     nemo iusto maiores.
+    <div class="m-5">
+      <Button label="Check" />
+    </div>
   </div>
   <router-view />
 </template>
+
+<script setup>
+import Button from "primevue/button";
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/aura-light-green/theme.css";
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue);
+
 app.mount("#app");
