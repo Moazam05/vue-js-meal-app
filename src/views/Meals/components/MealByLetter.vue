48f17a828d6929d0c2a4bcf3a6777c75166f3f38
Moazam05 <salmanmoazam08@gmail.com>
2024-02-29 17:39:37 +0500

    not found

--- src/views/Meals/components/MealByLetter.vue
+++ src/views/Meals/components/MealByLetter.vue
@@ -12,32 +12,50 @@
           }}</router-link>
         </div>
       </div>
-      <div class="grid grid-cols-3 sm:grid-cols-1 gap-6 py-6">
-        <div
-          v-for="meal in meals"
-          :key="meal.idMeal"
-          class="bg-white shadow-lg rounded-md"
-        >
-          <router-link :to="`/meal/${meal.idMeal}`">
-            <img
-              :src="meal.strMealThumb"
-              :alt="meal.strMeal"
-              class="rounded-t-md h-64 w-full object-cover cursor-pointer"
-            />
-          </router-link>
-          <div class="p-2">
-            <h3 class="font-semibold">{{ meal.strMeal }}</h3>
-            <p class="mt-2">{{ meal.strInstructions.slice(0, 100) }}...</p>
-
-            <a
-              :href="meal.strYoutube"
-              target="_blank"
-              class="block w-fit h-fit bg-transparent rounded-md border border-[#eb0014] p-2 text-[#eb0014] hover:bg-[#eb0014] hover:text-white text-sm text-center my-3"
-            >
-              Watch on YouTube
-            </a>
+      <div
+        :class="{
+          'grid-cols-1': meals === null,
+          'grid-cols-3': meals !== null,
+        }"
+        class="grid gap-6 py-6"
+      >
+        <template v-if="meals === null">
+          <div class="flex justify-center">
+            <div class="bg-white shadow-lg rounded-md p-4">
+              <p class="text-center">
+                No meals found. Please try a different letter.
+              </p>
+            </div>
           </div>
-        </div>
+        </template>
+
+        <template v-else>
+          <div
+            v-for="meal in meals"
+            :key="meal.idMeal"
+            class="bg-white shadow-lg rounded-md"
+          >
+            <router-link :to="`/meal/${meal.idMeal}`">
+              <img
+                :src="meal.strMealThumb"
+                :alt="meal.strMeal"
+                class="rounded-t-md h-64 w-full object-cover cursor-pointer"
+              />
+            </router-link>
+            <div class="p-2">
+              <h3 class="font-semibold">{{ meal.strMeal }}</h3>
+              <p class="mt-2">{{ meal.strInstructions.slice(0, 100) }}...</p>
+
+              <a
+                :href="meal.strYoutube"
+                target="_blank"
+                class="block w-fit h-fit bg-transparent rounded-md border border-[#eb0014] p-2 text-[#eb0014] hover:bg-[#eb0014] hover:text-white text-sm text-center my-3"
+              >
+                Watch on YouTube
+              </a>
+            </div>
+          </div>
+        </template>
       </div>
     </div>
   </div>
